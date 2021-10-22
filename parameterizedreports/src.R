4a6f1bf023c3508ab699a8deced7c2475ce18c8c
thiyangt <thiyanga.talagala@monash.edu>
2021-10-22 12:45:38 +0530

    updated

---
+++ parameterizedreports/src.R
@@ -0,0 +1,19 @@
+
+# Code to generate papers
+library(here)
+library(tidyverse)
+library(rmarkdown)
+
+
+
+saveid <- 1:2
+name <- c("xx", "yy")
+date <- c("2021-10-15", "2021-10-16")
+
+for (i in 1:2){
+  render(here("parameterizedreports", "letter.Rmd"), 
+         params=list(date = date[i], 
+                     name = name[i],
+                     index = saveid[i]),
+         output_file=paste0("index", saveid[i], ".pdf"))
+}
