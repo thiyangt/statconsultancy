c64e8e1a210240950ad278bdd3aad8cb583f2420
thiyangt <thiyanga.talagala@monash.edu>
2021-10-22 12:53:18 +0530

    Updated

--- parameterizedreports/src.R
+++ parameterizedreports/src.R
@@ -13,7 +13,6 @@ date <- c("2021-10-15", "2021-10-16")
 for (i in 1:2){
   render(here("parameterizedreports", "letter.Rmd"), 
          params=list(date = date[i], 
-                     name = name[i],
-                     index = saveid[i]),
+                     name = name[i]),
          output_file=paste0("index", saveid[i], ".pdf"))
 }
